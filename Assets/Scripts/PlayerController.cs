0b86d4cf8fc0212d0a484e8c2023143b9da44282
mtsgi <oq@live.jp>
2019-03-01 12:39:52 +0900

    Add PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10;
+
+    void FixedUpdate(){
+
+        // 入力をxとzに代入
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        // rigidbodyのx軸（横）とz軸（奥）に力を加える
+        rb.AddForce(x*speed, 0, z*speed);
+    }
+}
