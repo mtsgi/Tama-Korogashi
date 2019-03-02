3b1969ce01b67184e8d7bb057044429400d3ab13
mtsgi <oq@live.jp>
2019-03-03 07:13:13 +0900

    Add DangerWall

---
+++ Assets/Scripts/DangerWall.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DangerWall : MonoBehaviour{
+    public GameObject FC;
+    private void OnCollisionEnter(Collision col){
+        if( col.gameObject.CompareTag("Player") && !FC.activeSelf ) SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );       
+    }
+}
