79d501476f05db56403f79b6fb11ef42667a8a85
mtsgi <oq@live.jp>
2019-03-03 06:54:16 +0900

    Add GameController

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public UnityEngine.UI.Text SL;
+    public GameObject FC;
+
+    void Update () {
+        int count = GameObject.FindGameObjectsWithTag("Item").Length;
+        SL.text = count.ToString();
+
+        if( count == 0 ){
+            FC.SetActive(true);
+        }
+    }
+}
