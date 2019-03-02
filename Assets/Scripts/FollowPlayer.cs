28e6f989a669f9c052ee67a7dff5f4aae52958b3
mtsgi <oq@live.jp>
2019-03-02 22:30:10 +0900

    Add FollowPlayer Script

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public GameObject target;  // ターゲットへの参照
+    Vector3 offset;
+
+    void Start(){
+        //自分自身とtargetとの相対距離を求める
+        offset = GetComponent<Transform>().position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        GetComponent<Transform>().position = target.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.gameObject.transform.position + offset;
+    }
+}
