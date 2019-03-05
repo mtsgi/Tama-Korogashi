35ba2566556c8860b3c510912c5d2aac60591d03
mtsgi <oq@live.jp>
2019-03-06 07:43:18 +0900

    Add Bonus Stage

---
+++ Assets/Scripts/BonusBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BonusBox : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col){
+        if (col.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene("BonusScene");
+        }
+    }
+}
